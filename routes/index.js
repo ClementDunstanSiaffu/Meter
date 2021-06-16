b374921ecff75bbf599e2a50bb3fb611ce8b7821
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2021-06-16 20:55:37 +0300

    fifthy commit

--- routes/index.js
+++ routes/index.js
@@ -14,87 +14,111 @@ exports.get_tokens = (req,res)=>{
 
 exports.get_amount = async (req,res)=>{
     const passcode = req.params
-    const thatMeter = await Meter.findOne(passcode)
-    const amount = thatMeter.amount
-    res.status(200).json(amount)
+    if (passcode == 12345 || passcode == 67890){
+        const thatMeter = await Meter.findOne(passcode)
+        const amount = thatMeter.amount
+        res.status(200).json(amount)
+    }else{
+        res.status(200).json(0)
+    }
+  
 
 }
 
 exports.get_unit = async (req,res)=>{
-    const passcode = req.params
-    const thatMeter = await Meter.findOne(passcode)
-    const unit = thatMeter.unit
-    res.json(unit)
+    if (passcode == 12345 || passcode == 67890){
+        const passcode = req.params
+        const thatMeter = await Meter.findOne(passcode)
+        const unit = thatMeter.unit
+        res.json(unit)
+    }else{
+        res.json(0)
+    }
+   
 }
 
 exports.post_unit = async(req,res)=>{
     const obj = req.params
-    const meter = await Meter.findOne({passcode:obj.passcode})
-    const docs = await Meter.find((err,docs)=>{return docs})
-    const index = docs.findIndex((docs)=>docs.passcode == obj.passcode)
-    meter.unit = parseInt(obj.unit)
-    await Meter.replaceOne(docs[index],meter)
-    res.send("SUCCESS")
+    if (obj.passcode == 12345 || obj.passcode == 67890){
+        const meter = await Meter.findOne({passcode:obj.passcode})
+        const docs = await Meter.find((err,docs)=>{return docs})
+        const index = docs.findIndex((docs)=>docs.passcode == obj.passcode)
+        meter.unit = parseInt(obj.unit)
+        await Meter.replaceOne(docs[index],meter)
+        res.send("SUCCESS")
+    }else{
+        res.send("FAILED")
+    }
+   
 }
 
 exports.post_amount = async(req,res)=>{
     const obj = req.params
-    const meter = await Meter.findOne({passcode:obj.passcode})
-    const docs = await Meter.find((err,docs)=>{return docs})
-    const index = docs.findIndex((docs)=>docs.passcode == obj.passcode)
-    meter.amount = parseInt(obj.amount)
-    await Meter.replaceOne(docs[index],meter)
-    res.send("SUCCESS")
+    if(obj.passcode == 12345 || obj.passcode == 67890){
+        const meter = await Meter.findOne({passcode:obj.passcode})
+        const docs = await Meter.find((err,docs)=>{return docs})
+        const index = docs.findIndex((docs)=>docs.passcode == obj.passcode)
+        meter.amount = parseInt(obj.amount)
+        await Meter.replaceOne(docs[index],meter)
+        res.send("SUCCESS") 
+    }else{
+        res.send("FAILED") 
+    }
+    
 }
 
 exports.calculating_unit = async(req,res)=>{
     const obj = req.params
-    const meter = await Meter.findOne({passcode:obj.passcode})
-    const docs = await Meter.find((err,docs)=>{return docs})
-    const index = docs.findIndex((docs)=>docs.passcode == obj.passcode)
-    if (meter.amount > parseInt(obj.amount)){
-        meter.amount -= obj.amount
-        const unit = parseInt(obj.amount/300)
-        meter.unit = unit
-        let str_unit
-        if (unit >= 100 && unit <= 999){
-            str_unit = `0${unit}`
-        }else{
-            if (unit >= 1000){
-                str_unit = `${unit}`
+    if (obj.passcode == 12345 || obj.passcode == 67890){
+        const meter = await Meter.findOne({passcode:obj.passcode})
+        const docs = await Meter.find((err,docs)=>{return docs})
+        const index = docs.findIndex((docs)=>docs.passcode == obj.passcode)
+        if (meter.amount > parseInt(obj.amount)){
+            meter.amount -= obj.amount
+            const unit = parseInt(obj.amount/300)
+            meter.unit = unit
+            let str_unit
+            if (unit >= 100 && unit <= 999){
+                str_unit = `0${unit}`
             }else{
-                if (unit >=10 && unit <= 99){
-                    str_unit = `00${unit}`
+                if (unit >= 1000){
+                    str_unit = `${unit}`
                 }else{
-                    str_unit = `000${unit}`
+                    if (unit >=10 && unit <= 99){
+                        str_unit = `00${unit}`
+                    }else{
+                        str_unit = `000${unit}`
+                    }
+                   
                 }
                
             }
-           
-        }
-        if (obj.passcode == 12345){
-            const tokens = helper.arr_for_meterA()
-            const data = {
-                "tokens":tokens + " " + str_unit,
-                "unit":`${unit}`,
-                "status":"okay"
+            if (obj.passcode == 12345){
+                const tokens = helper.arr_for_meterA()
+                const data = {
+                    "tokens":tokens + " " + str_unit,
+                    "unit":`${unit}`,
+                    "status":"okay"
+                }
+                await Meter.replaceOne(docs[index],meter)
+                res.json(data)
+            }else{
+                const tokens = helper.arr_for_meterB()
+                const data = {
+                    "tokens":tokens + " " + str_unit,
+                    "unit":`${unit}`,
+                    "status":"okay"
+                }
+                await Meter.replaceOne(docs[index],meter)
+                res.json(data)
             }
-            await Meter.replaceOne(docs[index],meter)
-            res.send(`${data.tokens}`)
         }else{
-            const tokens = helper.arr_for_meterB()
-            const data = {
-                "tokens":tokens,
-                "unit":`${unit}`,
-                "status":"okay"
-            }
-            await Meter.replaceOne(docs[index],meter)
-            res.send(`${data.tokens}`)
+            res.json({"status":"insufficient balance"})
         }
     }else{
-        res.send(
-            "failed")
+        res.json({"status":"unmatch password"})
     }
+ 
     
 
     
